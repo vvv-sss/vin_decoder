c7178325198682e05589b2a269e498de9b629b6e
volodymyr_serbulenko <serbulenko1volodymyr@gmail.com>
2023-01-23 14:33:10 +0200

    Updated App component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import NavBar from "./home_page/navbar";
 import Home from './home_page/home';
 import About from './about_page/about';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <NavBar />
         <Routes>
-          <Route exact path="/" element={ <Home /> } />
+          <Route path="/" element={ <Home /> } />
           <Route path="/about" element={ <About /> } />
         </Routes>
       </div>
